ff115e0973d22747b9552634b53f116fcb4887ef
andwerpooni@gmail.com <andwerpooni@gmail.com>
2021-09-29 15:36:45 -0500

    Add project files.

---
+++ MyFirstProject/ViewViewModels/Main/MainPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MyFirstProject.Models;
+using MyFirstProject.ViewViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProject.ViewViewModels.Main
+{
+    public class MainPageViewModel : BaseViewModel
+    {
+        public MainPageViewModel()
+        {
+            Title = Titles.MainPageTitle;
+        }
+    }
+}
