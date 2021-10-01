208b3457a5e7eed2af99d1eec4be68ac2d629360
JacobSartin <59787306+JacobSartin@users.noreply.github.com>
2021-10-01 15:47:02 -0500

    Navigation

--- MyFirstProject/ViewViewModels/Main/MainPage.xaml.cs
+++ MyFirstProject/ViewViewModels/Main/MainPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using MyFirstProject.ViewViewModels.Listview;
+﻿using MyFirstProject.ViewViewModels.Controls;
+using MyFirstProject.ViewViewModels.Image;
+using MyFirstProject.ViewViewModels.Layouts;
+using MyFirstProject.ViewViewModels.Listview;
 using MyFirstProject.ViewViewModels.Main;
 using System;
 using System.Collections.Generic;
@@ -18,9 +21,21 @@ namespace MyFirstProject
             BindingContext = new MainPageViewModel();
         }
 
-        private async void DoSomething(object sender, EventArgs e)
+        private async void Lists(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new ListviewMenuView());
+        }
+        private async void Controls(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ControlsMenuView());
+        }
+        private async void Images(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ImageMenuView());
+        }
+        private async void Layouts(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LayoutsMenuView());
         }
     }
 }
