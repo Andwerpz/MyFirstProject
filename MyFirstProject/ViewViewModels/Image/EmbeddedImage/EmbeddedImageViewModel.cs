d70667d9403445320c3b75a4581407aff79581c8
JacobSartin <59787306+JacobSartin@users.noreply.github.com>
2021-10-21 15:22:12 -0500

    Image

--- MyFirstProject/ViewViewModels/Image/EmbeddedImage/EmbeddedImageViewModel.cs
+++ MyFirstProject/ViewViewModels/Image/EmbeddedImage/EmbeddedImageViewModel.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Text;
 using MyFirstProject.Models;
 using MyFirstProject.ViewViewModel;
+using Xamarin.Forms;
 
 namespace MyFirstProject.ViewViewModels.Image.EmbeddedImage
 {
     public class EmbeddedImageViewModel : BaseViewModel
     {
+        public ImageSource EmbeddedImageSrc { get; set; }
         public EmbeddedImageViewModel()
         {
             Title = Titles.EmbeddedImageTitle;
+            this.GetEmbeddedImageSrc();
+        }
+
+        private void GetEmbeddedImageSrc()
+        {
+            EmbeddedImageSrc = ImageSource.FromResource("MyFirstProject.Images.nut.jpg");
         }
     }
 }
