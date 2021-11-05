a4dcb7307ca42735c0e7efd220be0d6a9759f1f5
JacobSartin <59787306+JacobSartin@users.noreply.github.com>
2021-11-05 16:01:13 -0500

    Person model

---
+++ MyFirstProject/Models/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProject.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string n)
+        {
+            Name = n;
+        }
+
+        public static List<Person> GetPerson()
+        {
+            return new List<Person>
+            {
+                new Person("Chris Pratt"),
+                new Person("Zoe Saldana"),
+                new Person("Vin Diesel"),
+                new Person("Bradely Cooper"),
+                new Person("Karen Gillan"),
+                new Person("Ophelia Lovibond"),
+                new Person("Laura Haddock")
+            };
+        }
+    }
+}
