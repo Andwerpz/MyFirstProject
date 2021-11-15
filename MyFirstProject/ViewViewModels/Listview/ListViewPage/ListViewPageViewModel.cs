e41bd159b2da004883f69057cd731dd69951fbaa
JacobSartin <59787306+JacobSartin@users.noreply.github.com>
2021-11-15 15:46:38 -0600

    did it

--- MyFirstProject/ViewViewModels/Listview/ListViewPage/ListViewPageViewModel.cs
+++ MyFirstProject/ViewViewModels/Listview/ListViewPage/ListViewPageViewModel.cs
@@ -2,16 +2,41 @@
 using MyFirstProject.ViewViewModel;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Text;
 
 namespace MyFirstProject.ViewViewModels.Listview.ListViewPage
 {
     class ListViewPageViewModel : BaseViewModel
     {
+        public ObservableCollection<Person> Persons {  get; }
+
+        private List<Person> _person;
         public ListViewPageViewModel()
         {
             Title = Titles.ListViewPageTitle;
+            Persons = new ObservableCollection<Person>();
+            _person =Person.GetPerson();
+            this.LoadPersons();
+        }
+
+        private void LoadPersons()
+        {
+            try
+            {
+                Persons.Clear();
+                foreach (var p in _person)
+                {
+                    Persons.Add(p);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
         }
         
     }
 }
+
