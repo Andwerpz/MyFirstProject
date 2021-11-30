3a68c7d1a816154d3fb1cb2b3340ec7e3ebab6aa
JacobSartin <59787306+JacobSartin@users.noreply.github.com>
2021-11-30 16:04:58 -0600

    nut list

---
+++ MyFirstProject/Models/NutList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProject.Models
+{
+    internal class NutList
+    {
+        public string Nut { get; set; }
+
+        public NutList()
+        {
+        }
+        public NutList(string n)
+        {
+            Nut = n;
+        }
+
+        public static List<NutList> GetNutList()
+        {
+            return new List<NutList>
+            {
+                new NutList(""),
+                new NutList(""),
+                new NutList(""),
+                new NutList(""),
+                new NutList(""),
+                new NutList(""),
+                new NutList(""),
+                new NutList("")
+            };
+        }
+}
